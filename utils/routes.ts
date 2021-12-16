d15fd94475917f2d6759f298f5663f25a39b69cb
Benyamin Ahmed <benyaminahmed2@gmail.com>
2021-12-16 12:02:41 +0000

    Changed Discord link to correct server

--- utils/routes.ts
+++ utils/routes.ts
@@ -10,6 +10,6 @@ export const routes = {
     openSeaWhaleBasePath:
       'https://opensea.io/assets/0x96ed81c7f4406eff359e27bff6325dc3c9e042bd/',
     WWTwitter: 'https://twitter.com/WeirdWhales',
-    WWDiscord: 'https://discord.com/invite/gpSU5AVjju',
+    WWDiscord: 'https://discord.gg/UJ348eqzsj',
   },
 } as const;
