251d6d2c87f07f462195355b3afcb4906c7b429f
Benyamin Ahmed <benyaminahmed2@gmail.com>
2022-02-16 10:14:46 +0000

    Added search bar with no functionalit

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,20 @@ type ServerHydratedProps = {
   randomWhaleIDs: number[];
 };
 
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=site%3A+';
+const site = 'pagedart.com';
+
+const submitted = (event) => {
+  event.preventDefault();
+  const url = google + site + '+' + q.value;
+  const win = window.open(url, '_blank') | null;
+  win.focus();
+};
+
+f.addEventListener('submit', submitted);
+
 const Home: NextPage<ServerHydratedProps> = ({ randomWhaleIDs }) => {
   return (
     <div>
@@ -346,6 +360,33 @@ const Home: NextPage<ServerHydratedProps> = ({ randomWhaleIDs }) => {
                 .
               </p>
             </FlexRow>
+          </Container>
+        </Section>
+        <Section>
+          <hr className={styles.grayLine} />
+        </Section>
+        <Section>
+          <Container>
+            <h2 className={styles.subTitle}>SEARCH WHALES</h2>
+
+            <form role="search" className={styles.searchBar} id="form">
+              <input
+                className={styles.searchBarInput}
+                type="search"
+                id="query"
+                name="q"
+                placeholder="Search..."
+                aria-label="Search through site content"
+              />
+              <button className={styles.searchBarButton}>
+                <svg className={styles.searchBarSvg} viewBox="0 0 1024 1024">
+                  <path
+                    className={styles.path1}
+                    d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"
+                  ></path>
+                </svg>
+              </button>
+            </form>
           </Container>
         </Section>
       </main>
